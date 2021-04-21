9a1e535731c1f24d54f5dde7d32a5b9b3972b88e
Alex <cigan@ua.fm>
2021-04-21 15:38:00 +0300

    sk001: add a dublicate method to prototype of Array

---
+++ duplicateArr.js
@@ -0,0 +1,7 @@
+// "Добавить такое поведение массиву:
+// const a = [1, 2, 3, 4, 5];
+// a.duplicate(); //1,2,3,4,5,1,2,3,4,5"
+
+Array.prototype.duplicate = function () {
+  return this + ',' + this;
+};
